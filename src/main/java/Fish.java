f2889fd5ce75e853625d979440a3f01577030815
rando <rando@DESKTOP-5TAVSM1>
2021-05-02 11:54:50 -0500

    My first commit for fishing sim

---
+++ src/main/java/Fish.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rando
+ */
+public class Fish {
+    private String name;
+    private String location;
+    private String rarity;
+
+    public Fish(String name, String location, String rarity) {
+        this.name = name;
+        this.location = location;
+        this.rarity = rarity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getRarity() {
+        return rarity;
+    }
+
+    public void setRarity(String rarity) {
+        this.rarity = rarity;
+    }
+    
+    
+
+}
